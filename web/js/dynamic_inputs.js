dda8402ced355bad4454b499e0223c23b661bc69
Yeq6X <orange.m0912@gmail.com>
2025-09-16 21:10:50 +0900

    修正

--- web/js/dynamic_inputs.js
+++ web/js/dynamic_inputs.js
@@ -8,22 +8,22 @@ app.registerExtension({
             case "ImageFrameSelector": // backward compatibility
                 nodeType.prototype.onNodeCreated = function () {
                     this._imageType = "IMAGE"
-                    
-                    const updateButton = this.addWidget("button", "Update inputs", null, () => {
+
+                    const updateInputs = () => {
                         if (!this.inputs) {
                             this.inputs = [];
                         }
                         if (!this.widgets) {
                             this.widgets = [];
                         }
-                        
+
                         const target_number_of_inputs = this.widgets.find(w => w.name === "inputcount")["value"];
                         const num_image_inputs = this.inputs.filter(input => input.type === this._imageType).length;
-                        
+
                         if(target_number_of_inputs === num_image_inputs) {
                             return; // already set, do nothing
                         }
-                        
+
                         // Remove excess inputs
                         if(target_number_of_inputs < num_image_inputs) {
                             const inputs_to_remove = num_image_inputs - target_number_of_inputs;
@@ -37,12 +37,12 @@ app.registerExtension({
                                 }
                             }
                         }
-                        
+
                         // Add new inputs
                         for(let i = num_image_inputs + 1; i <= target_number_of_inputs; ++i) {
                             this.addInput(`image_${i}`, this._imageType);
                         }
-                        
+
                         // Update inputsボタンを最後尾に移動
                         if (updateButton) {
                             const buttonIndex = this.widgets.indexOf(updateButton);
@@ -51,7 +51,65 @@ app.registerExtension({
                                 this.widgets.push(updateButton);
                             }
                         }
-                    });
+                    };
+
+                    const updateButton = this.addWidget("button", "Update inputs", null, updateInputs);
+
+                    // ワークフロー読み込み時の初期化
+                    setTimeout(() => {
+                        updateInputs();
+                        // ノードサイズを強制更新
+                        this.setDirtyCanvas(true, true);
+                        this.setSize?.(this.computeSize());
+                        // 追加の更新処理
+                        if (this.graph && this.graph.canvas) {
+                            this.graph.canvas.setDirty(true, true);
+                        }
+                    }, 100);
+                }
+
+                // ワークフロー読み込み後の処理も追加
+                const originalConfigure = nodeType.prototype.configure;
+                nodeType.prototype.configure = function(info) {
+                    originalConfigure?.call(this, info);
+                    // configure後に入力数を調整
+                    setTimeout(() => {
+                        if (this.widgets) {
+                            const inputcountWidget = this.widgets.find(w => w.name === "inputcount");
+                            if (inputcountWidget) {
+                                const target_number_of_inputs = inputcountWidget.value;
+                                const num_image_inputs = this.inputs ? this.inputs.filter(input => input.type === this._imageType).length : 0;
+
+                                if(target_number_of_inputs !== num_image_inputs) {
+                                    // Remove excess inputs
+                                    if(target_number_of_inputs < num_image_inputs) {
+                                        const inputs_to_remove = num_image_inputs - target_number_of_inputs;
+                                        for(let i = 0; i < inputs_to_remove; i++) {
+                                            for(let j = this.inputs.length - 1; j >= 0; j--) {
+                                                if(this.inputs[j].type === this._imageType) {
+                                                    this.removeInput(j);
+                                                    break;
+                                                }
+                                            }
+                                        }
+                                    }
+                                    // Add new inputs
+                                    else {
+                                        for(let i = num_image_inputs + 1; i <= target_number_of_inputs; ++i) {
+                                            this.addInput(`image_${i}`, this._imageType);
+                                        }
+                                    }
+                                    // ノードサイズを強制更新
+                                    this.setDirtyCanvas(true, true);
+                                    this.setSize?.(this.computeSize());
+                                    // 追加の更新処理
+                                    if (this.graph && this.graph.canvas) {
+                                        this.graph.canvas.setDirty(true, true);
+                                    }
+                                }
+                            }
+                        }
+                    }, 50);
                 }
                 break;
             case "WanVideoLatentInsertFrames":
